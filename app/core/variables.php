a39b2390e42b9cfc5541cd372e752d969adfacd5
Kaye Jacildo <58097285+kaiyeej@users.noreply.github.com>
2023-10-22 22:06:07 +0800

    Update variables.php

--- app/core/variables.php
+++ app/core/variables.php
@@ -1,14 +1,14 @@
 <?php
 /** Core Path **/
-define("host","localhost");
-define("username","root");
-define("password","");
-define("database","water_billing_db");
-
 // define("host","localhost");
-// define("username","u814036432_gecmath_root");
-// define("password","gecMath123!");
-// define("database","u814036432_gecmath_db");
+// define("username","root");
+// define("password","");
+// define("database","water_billing_db");
+
+define("host","localhost");
+define("username","u814036432_wb_root");
+define("password","oVpn[Z4Oi?");
+define("database","u814036432_water_billing");
 
 //inside dir
 define ("VALUE",serialize (array("","my_functions.php")));
