b17c4b2281f17c686cb4ffc516d10c33a6bb9e5b
Kaye Jacildo <58097285+kaiyeej@users.noreply.github.com>
2023-10-24 16:14:23 +0800

    Update getReadings.php

--- app/getReadings.php
+++ app/getReadings.php
@@ -14,7 +14,18 @@ $user_id = $mysqli_connect->real_escape_string($data->user_id);
 $fetch = $mysqli_connect->query("SELECT * FROM tbl_bills WHERE encoded_by='$user_id'") or die(mysql_error());
 $response = array();
 while ($row = $fetch->fetch_array()) {
-    $total = ($row['current_reading']-$row['previous_reading'])*$row['cubic_meter_rate'];
+
+    $dateMonth = date('m', strtotime($row['billing_date']));
+    $dateYear = date('Y', strtotime($row['billing_date']));
+
+    // Calculate the date for the last month
+    $lastMonth = date('Y-m', strtotime('-1 month', strtotime($date)));
+
+    // Extract the year and month
+    $lastMonthYear = date('Y', strtotime($lastMonth));
+    $lastMonthMonth = date('m', strtotime($lastMonth));
+
+    $total = ($row['current_reading'] - $row['previous_reading']) * $row['cubic_meter_rate'];
     $list = array();
     $list['id'] = $row['bill_id'];
     $list['previous_reading'] = $row['previous_reading'];
@@ -23,11 +34,12 @@ while ($row = $fetch->fetch_array()) {
     $list['cubic_meter_rate'] = $row['cubic_meter_rate'];
     $list['penalty_amount'] = $row['penalty_amount'];
     $list['total'] = $total;
-    $list['amount'] = number_format($total,2);//$row['penalty_amount'];
+    $list['amount'] = number_format($total, 2); //$row['penalty_amount'];
     $list['billing_date'] = $row['billing_date'];
+    $row['billing_period'] = date('M', mktime(0, 0, 0, $lastMonthMonth, 1)) . " " . $lastMonthYear . " to " . date('M', mktime(0, 0, 0, $dateMonth, 1)) . " " . $dateYear;
     $list['due_date'] = $row['due_date'];
     $list['status'] = $row['status'];
-    $list['date_added'] = $row['date_added'];   
+    $list['date_added'] = $row['date_added'];
     array_push($response, $list);
 }
 
