18eb6986d7c93281482a8bd5e0e5ff8e72e1ea16
Kaye Jacildo <58097285+kaiyeej@users.noreply.github.com>
2023-10-26 11:28:25 +0800

    Update getBilling.php

--- app/getBilling.php
+++ app/getBilling.php
@@ -15,7 +15,11 @@ $user_id = $mysqli_connect->real_escape_string($data->id);
 
 $row = array();
 $fetch = $mysqli_connect->query("SELECT * FROM tbl_bills where user_id='$user_id' ORDER BY bill_id DESC") or die(mysql_error());
-$row = $fetch->fetch_array();
+if($fetch->num_rows > 0){
+	$row = $fetch->fetch_array();
+}else{
+	$row = 0;
+}
 
 $date = getCurrentDate();
 $dateMonth = date('m', strtotime($date));
