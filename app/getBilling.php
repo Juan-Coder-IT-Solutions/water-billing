4a3ac1905e95dc4fda549d0b9287192cc9d2fcc9
Kaye Jacildo <58097285+kaiyeej@users.noreply.github.com>
2023-10-26 10:49:17 +0800

    Update getBilling.php

--- app/getBilling.php
+++ app/getBilling.php
@@ -11,20 +11,32 @@ $data = json_decode(file_get_contents("php://input"));
 
 // if(isset($data->id) && !empty($data->id )){
 
-	$user_id = $mysqli_connect->real_escape_string($data->id);
+$user_id = $mysqli_connect->real_escape_string($data->id);
 
-	$response = array();
-	$fetch = $mysqli_connect->query("SELECT * FROM tbl_bills where user_id='$user_id' ORDER BY bill_id DESC") or die(mysql_error());
-	$response = $fetch->fetch_array();
+$row = array();
+$fetch = $mysqli_connect->query("SELECT * FROM tbl_bills where user_id='$user_id' ORDER BY bill_id DESC") or die(mysql_error());
+$row = $fetch->fetch_array();
 
-    $param = getParam(getCustomerType($user_id));
-    $response['cubic_meter_rate'] = $param['cubic_meter_rate'];
-    $response['maximum_cubic'] = $param['maximum_cubic'];
-    $response['minimum_rate'] = $param['minimum_rate'];
-    
 
-	echo json_encode($response);
+$date = getCurrentDate();
+$dateMonth = date('m', strtotime($date));
+$dateYear = date('Y', strtotime($date));
+
+// Calculate the date for the last month
+$lastMonth = date('Y-m', strtotime('-1 month', strtotime($date)));
+
+// Extract the year and month
+$lastMonthYear = date('Y', strtotime($lastMonth));
+$lastMonthMonth = date('m', strtotime($lastMonth));
+
+
+$param = getParam(getCustomerType($user_id));
+$row['billing_period'] = date('M', mktime(0, 0, 0, $lastMonthMonth, 1)) . " " . $lastMonthYear . " to " . date('M', mktime(0, 0, 0, $dateMonth, 1)) . " " . $dateYear;
+$row['cubic_meter_rate'] = $param['cubic_meter_rate'];
+$row['maximum_cubic'] = $param['maximum_cubic'];
+$row['minimum_rate'] = $param['minimum_rate'];
+
+
+echo json_encode($row);
 
 // }
-
-?>
