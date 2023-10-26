13789e07d1526b22ff35b15a0326a5004bfadf52
Kaye Jacildo <58097285+kaiyeej@users.noreply.github.com>
2023-10-26 11:38:30 +0800

    Update getBilling.php

--- app/getBilling.php
+++ app/getBilling.php
@@ -26,13 +26,6 @@ $lastMonthYear = date('Y', strtotime($lastMonth));
 $lastMonthMonth = date('m', strtotime($lastMonth));
 
 
-$param = getParam(getCustomerType($user_id));
-$row['billing_period'] = date('M', mktime(0, 0, 0, $lastMonthMonth, 1)) . " " . $lastMonthYear . " to " . date('M', mktime(0, 0, 0, $dateMonth, 1)) . " " . $dateYear;
-$row['cubic_meter_rate'] = $param['cubic_meter_rate'];
-$row['maximum_cubic'] = $param['maximum_cubic'];
-$row['minimum_rate'] = $param['minimum_rate'];
-$row['penalty_amount'] = $param['late_penalty_amount'];
-
 $row = array();
 $fetch = $mysqli_connect->query("SELECT * FROM tbl_bills where user_id='$user_id' ORDER BY bill_id DESC") or die(mysql_error());
 if($fetch->num_rows > 0){
@@ -40,6 +33,14 @@ if($fetch->num_rows > 0){
 }else{
 	$row['current_reading'] = 0;
 }
+
+$param = getParam(getCustomerType($user_id));
+$row['billing_period'] = date('M', mktime(0, 0, 0, $lastMonthMonth, 1)) . " " . $lastMonthYear . " to " . date('M', mktime(0, 0, 0, $dateMonth, 1)) . " " . $dateYear;
+$row['cubic_meter_rate'] = $param['cubic_meter_rate'];
+$row['maximum_cubic'] = $param['maximum_cubic'];
+$row['minimum_rate'] = $param['minimum_rate'];
+$row['penalty_amount'] = $param['late_penalty_amount'];
+
 
 echo json_encode($row);
 
