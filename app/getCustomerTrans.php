510beab8416e27bc09457ca1283654c3b0e5274a
Kaye Jacildo <58097285+kaiyeej@users.noreply.github.com>
2023-11-29 16:05:32 +0800

    app

--- app/getCustomerTrans.php
+++ app/getCustomerTrans.php
@@ -24,8 +24,6 @@ while ($row = $fetch->fetch_array()) {
     // Extract the year and month
     $lastMonthYear = date('Y', strtotime($lastMonth));
     $lastMonthMonth = date('m', strtotime($lastMonth));
-    $amount = $get_billing_total_balance($row['bill_id']);
-    $amount_due = $amount+$row['penalty_amount'];
 
     $total = ($row['current_reading'] - $row['previous_reading']) * $row['cubic_meter_rate'];
     $list = array();
@@ -36,8 +34,8 @@ while ($row = $fetch->fetch_array()) {
     $list['cubic_meter_rate'] = $row['cubic_meter_rate'];
     $list['penalty_amount'] = $row['penalty_amount'];
     $list['total'] = $total;
-    $list['amount'] = number_format($amount, 2); //$row['penalty_amount'];
-    $list['amount_due'] = number_format($amount_due, 2); //$row['penalty_amount'];
+    $list['amount'] = number_format($total, 2); //$row['penalty_amount'];
+    $list['amount_due'] = number_format($total+$row['penalty_amount'], 2);
     $list['billing_date'] = $row['billing_date'];
     $list['billing_period'] = date('M', mktime(0, 0, 0, $lastMonthMonth, 1)) . " " . $lastMonthYear . " to " . date('M', mktime(0, 0, 0, $dateMonth, 1)) . " " . $dateYear;
     $list['due_date'] = date("F j, Y h:i A",strtotime($row['due_date']));
